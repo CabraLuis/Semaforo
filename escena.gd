a30bbf2379d17d24f47424c07be129e7e3beb854
Luis Cabra <zetinaluis03@gmail.com>
2024-04-11 20:35:39 -0500

    a

---
+++ escena.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_btn_iniciar_pressed():
+	pass
+	
+func _on_btn_detener_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_preventivas_pressed():
+	pass # Replace with function body.
