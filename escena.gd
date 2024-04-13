01865b5cd00138be3071c30e4463b38203762677
Luis Cabra <zetinaluis03@gmail.com>
2024-04-12 21:22:10 -0500

    xd

--- escena.gd
+++ escena.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var semaforo = 1
-var time = 0.0
+var time = 1.0
 var seconds: int = 0
 var start = false
 
@@ -19,43 +19,71 @@ func _process(delta):
 
 func timer_Green():
 	get_node("Semaforo"+str(semaforo)+"/Rojo").visible = true
+	get_node("Semaforo"+str(semaforo+1)+"/Rojo").visible = true
+	get_node("Timer").visible = false
 	await get_tree().create_timer(0.5).timeout	
-	time = 0.0
-	get_node("Semaforo"+str(semaforo)+"/Verde").label_settings.font_color = Color(0,255,0)
+	time = 1.0
+	get_node("Timer").label_settings.font_color = Color(0,255,0)
 	get_node("Semaforo"+str(semaforo)+"/Verde").visible = false
+	get_node("Semaforo"+str(semaforo+1)+"/Verde").visible = false
+	get_node("Timer").visible = true	
 	await get_tree().create_timer(19.5).timeout
 	get_node("Semaforo"+str(semaforo)+"/Verde").visible = true
+	get_node("Semaforo"+str(semaforo+1)+"/Verde").visible = true
+	get_node("Timer").visible = false
 	await get_tree().create_timer(0.5).timeout
+	time = 1.0
 	get_node("Semaforo"+str(semaforo)+"/Verde").visible = false
-	await get_tree().create_timer(0.5).timeout
-	time = 0.0
-	get_node("Semaforo"+str(semaforo)+"/Verde").visible = true
-	await get_tree().create_timer(0.5).timeout
-	get_node("Semaforo"+str(semaforo)+"/Verde").visible = false
-	await get_tree().create_timer(0.5).timeout
-	get_node("Semaforo"+str(semaforo)+"/Verde").visible = true
-	await get_tree().create_timer(0.5).timeout
-	get_node("Semaforo"+str(semaforo)+"/Verde").visible = false
+	get_node("Semaforo"+str(semaforo+1)+"/Verde").visible = false
+	get_node("Timer").visible = true
 	await get_tree().create_timer(0.5).timeout
 	get_node("Semaforo"+str(semaforo)+"/Verde").visible = true
+	get_node("Semaforo"+str(semaforo+1)+"/Verde").visible = true
+	get_node("Timer").visible = false
+	await get_tree().create_timer(0.5).timeout
+	get_node("Semaforo"+str(semaforo)+"/Verde").visible = false
+	get_node("Semaforo"+str(semaforo+1)+"/Verde").visible = false
+	get_node("Timer").visible = true
+	await get_tree().create_timer(0.5).timeout
+	get_node("Semaforo"+str(semaforo)+"/Verde").visible = true
+	get_node("Semaforo"+str(semaforo+1)+"/Verde").visible = true
+	get_node("Timer").visible = false	
+	await get_tree().create_timer(0.5).timeout
+	get_node("Semaforo"+str(semaforo)+"/Verde").visible = false
+	get_node("Semaforo"+str(semaforo+1)+"/Verde").visible = false
+	get_node("Timer").visible = true	
+	await get_tree().create_timer(0.5).timeout
 
 func timer_Yellow():
-	time = 0.0
+	get_node("Semaforo"+str(semaforo)+"/Verde").visible = true
+	get_node("Semaforo"+str(semaforo+1)+"/Verde").visible = true
+	get_node("Timer").visible = false	
+	await get_tree().create_timer(0.5).timeout
+	time = 1.0
+	get_node("Timer").label_settings.font_color = Color(255,255,0)
 	get_node("Semaforo"+str(semaforo)+"/Amarillo").visible = false
+	get_node("Semaforo"+str(semaforo+1)+"/Amarillo").visible = false
+	get_node("Timer").visible = true	
 	await get_tree().create_timer(2.5).timeout
-	get_node("Semaforo"+str(semaforo)+"/Amarillo").visible = true
 
 func timer_Red():
-	time = 0.0
+	get_node("Semaforo"+str(semaforo)+"/Amarillo").visible = true
+	get_node("Semaforo"+str(semaforo+1)+"/Amarillo").visible = true
+	get_node("Timer").visible = false	
 	await get_tree().create_timer(0.5).timeout
+	time = 1.0
+	get_node("Timer").label_settings.font_color = Color(255,0,0)
 	get_node("Semaforo"+str(semaforo)+"/Rojo").visible = false
+	get_node("Semaforo"+str(semaforo+1)+"/Rojo").visible = false
+	get_node("Timer").visible = true	
 	await get_tree().create_timer(1.5).timeout
-	semaforo = semaforo + 1
+	semaforo = semaforo + 2
 
 
 func _on_btn_iniciar_pressed():
 	start = true
-	get_node("Semaforo1/Rojo").visible = true
+	get_node("Semaforo"+str(semaforo)+"/Rojo").visible = true
+	get_node("Semaforo"+str(semaforo+1)+"/Rojo").visible = true
 	while start:
 		if semaforo > 4:
 			semaforo = 1
